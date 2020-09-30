cda3333af9ef8e9a3f6002ab2bbc900c6a65a59a
mei-riko <dobrovolska.marina@gmail.com>
2020-09-30 17:20:25 +0300

    product adaptive

--- src/component/slider/slider.js
+++ src/component/slider/slider.js
@@ -20,7 +20,30 @@ $(document).ready(() =>{
 			slidesToScroll: 4,
 			arrows        : true,
 			dots          : false,
-      autoplay      : false
+      autoplay      : false,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        }
+      ]
     });
     // Slider Product
     let $slickProduct = $('.slider.slider_product');
@@ -30,7 +53,30 @@ $(document).ready(() =>{
 			slidesToScroll: 5,
 			arrows        : true,
 			dots          : false,
-      autoplay      : false
+      autoplay      : false,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        }
+      ]
     });
     // Slider Card
     $('.product-slider').slick({
@@ -46,7 +92,30 @@ $(document).ready(() =>{
       asNavFor: '.product-slider',
       dots: false,
       centerMode: true,
-      focusOnSelect: true
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        }
+      ]
     });
   }
 });
